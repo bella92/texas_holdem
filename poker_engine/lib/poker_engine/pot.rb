f18012d3342ef19a938c4b7a02209089465ee308
bella92 <bella_n@abv.bg>
2014-02-19 01:46:15 +0200

    Add Pot class

---
+++ poker_engine/lib/poker_engine/pot.rb
@@ -0,0 +1,43 @@
+class Pot
+  attr_reader :pot
+
+  def initialize
+    @pot = Hash.new { 0 }
+    @all_in_cap = 0
+  end
+
+  def add_bet(player, amount)
+    @pot[player] += amount
+    @all_in_cap = @pot[player] if player.all_in? and @all_in_cap.zero?
+    overage unless @all_in_cap.zero?
+  end
+
+  def players_betted_cap?
+    @pot.values.all? { |bet| bet == @all_in_cap } && !@all_in_cap.zero?
+  end
+
+  def players_betted_same?
+    @pot.values.uniq.size == 1
+  end
+
+  def highest_bet
+    @pot.values.max
+  end
+
+  def amount
+    @pot.inject{|sum, bet| sum + bet }
+  end
+
+  def empty
+    @pot = {}
+  end
+
+  private
+
+  def overage
+    @pot.keys.select { |player| player > @all_in_cap }.inject({}) do |overage, player|
+      overage[player] = @pot
+      @pot[player] = @all_in_cap
+      overage
+    end
+  end
