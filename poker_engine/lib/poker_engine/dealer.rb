4241a8d9d6822dc2d54f891b7c6d32d9a50c714c
bella92 <bella_n@abv.bg>
2014-02-21 23:38:44 +0200

    Dealer ready

--- poker_engine/lib/poker_engine/dealer.rb
+++ poker_engine/lib/poker_engine/dealer.rb
@@ -1,24 +1,23 @@
 require_relative "deck.rb"
 
 class Dealer
-  attr_reader :deck, :table
+  attr_reader :deck
 
-  def initialize(table)
+  def initialize()
     @deck = Deck.new
-    @table = table
   end
 
-  def deal_preflop(players)
+  def deal_preflop(table)
     2.times do
-      players.each do |player|
+      table.players.each do |player|
         cards = @deck.take_cards(1)
         player.add_cards_to_pocket(cards)
       end
     end
   end
 
-  def deal_to_board(count)
+  def deal_to_board(table, count)
     cards = @deck.take_cards(count, skip: 1)
-    @table.add_cards_to_board(cards)
+    table.add_cards_to_board(cards)
   end
 end
